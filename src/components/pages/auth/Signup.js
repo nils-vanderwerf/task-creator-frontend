84c5c56d20bb4d51e471a06c1fdc115554d815d6
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-11 21:37:08 +1000

    Formik Added

--- src/components/pages/auth/Signup.js
+++ src/components/pages/auth/Signup.js
@@ -3,63 +3,116 @@ import { Jumbotron } from 'react-bootstrap'
 import { useDispatch } from 'react-redux';
 import userActions from '../../../redux/actions/userActions';
 import './Form.style.css'
-import { Button, Form } from 'react-bootstrap'
-import { FormGroup } from '@material-ui/core';
+import { Button, Form} from 'react-bootstrap'
+// import { FormGroup } from '@material-ui/core';
+import { validationSchema } from '../../../Validations/ValidationSchema';
+// import * as yup from "yup"
+import { useFormik } from 'formik';
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
 const Signup = props => {
   // initializing dispatch
   const dispatch = useDispatch();
 
   // Setting up local state using the useState hook
-  const [signupForm, setSignupForm] = useState({
-    user: {
+  // const [signupForm, setSignupForm] = useState({
+  //   user: {
+  //     email: '',
+  //     password: '',
+  //     first_name: '',
+  //     last_name: ''
+  //   }
+  // });
+
+  const formik = useFormik({
+    initialValues: {
       email: '',
       password: '',
       first_name: '',
       last_name: ''
+    },
+    onSubmit: values => {
+      const formData = { 
+        user: values
     }
+    submitForm(formData)
+  },
+    validationSchema
   });
 
-  // Controlled form functions
-  const handleChange = e => {
+//   const validationSchema = yup.object().shape({
+//     first_name: yup
+//           .string()
+//           .min(2, "Too short!")
+//           .max(50, "Too long!")
+//           .required("Required"),
+//     last_name: yup
+//           .string()
+//           .min(2, "Too short!")
+//           .max(50, "Too long!")
+//           .required("Required"),
+//     email: yup
+//           .string()
+//           .email("Invalid email")
+//           .required("Required"),
+//     password: yup
+//           .string()
+//           .min(4, "Too short!")
+//           .max(20, "Too long!")
+//           .required("Required")
+// })
 
-    setSignupForm({
-      ...signupForm, user: {
-        ...signupForm.user,
-        [e.target.name]: e.target.value
-      }
-    }
-    );
-    console.log("Signup form", signupForm)
-  }
 
-  const handleSubmit = e => {
-    e.preventDefault();
+
+
+  // // Controlled form functions
+  // const handleChange = e => {
+  //   setSignupForm({
+  //     ...signupForm, user: {
+  //       ...signupForm.user,
+  //       [e.target.name]: e.target.value
+  //     }
+  //   }
+  //   );
+  //   console.log("Signup form", signupForm)
+  // }
+
+  const submitForm = (formData) => {
     const { history } = props;
-    console.log("Sign up data: ", signupForm)
-    dispatch(userActions.newUserToDB(signupForm));
+    dispatch(userActions.newUserToDB(formData));
     history.push('/');
   };
 
-  // Destructuring keys from our local state to use in the form
-  console.log("destructured:", signupForm.user)
-  const { email, password, first_name, last_name } = signupForm.user;
+  // // Destructuring keys from our local state to use in the form
+  // console.log("destructured:", signupForm.user)
+  // const { email, password, first_name, last_name } = signupForm.user;
 
   // Component code
   return (
     <div className="auth-form col-12">
        <div className="form-inner-content align-items-center justify-content-center col-sm-4">
       <h1 className="auth-header mb-4">Create a new account</h1>
+
      
-      <Form className="form" onSubmit={handleSubmit}>
+      <Form className="form" onSubmit={formik.handleSubmit}>
       <Form.Group className="mb-3">
           <Form.Label>First Name</Form.Label>
           <Form.Control
             type="text"
             name="first_name"
             placeholder="Enter First Name"
-            value={first_name}
-            onChange={handleChange}
+            value={formik.values.first_name}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
           />
+           {formik.touched.first_name && formik.errors.first_name ? 
+           (
+            <p className="error-message">
+              <strong>{formik.errors.first_name}</strong>
+            </p>
+          ) : null}
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Last Name</Form.Label>
@@ -67,9 +120,14 @@ const Signup = props => {
             type="text"
             name="last_name"
             placeholder="Enter Last Name"
-            value={last_name}
-            onChange={handleChange}
+            value={formik.values.last_name}
+            onChange={formik.handleChange}
           />
+          {formik.touched.last_name && formik.errors.last_name ? (
+         <p className="error-message">
+           <strong>{formik.errors.last_name}</strong>
+           </p>
+       ) : null}
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
@@ -77,21 +135,30 @@ const Signup = props => {
             type="text"
             name="email"
             placeholder="Enter Email"
-            value={email}
-            onChange={handleChange}
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
+             {formik.touched.email && formik.errors.email ? (
+         <p className="error-message">
+           <strong>{formik.errors.email}</strong>
+          </p>
+       ) : null}
         </Form.Group>
-
+     
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             name="password"
             placeholder="Enter Password"
-            value={password}
-            onChange={handleChange}
+            value={formik.values.password}
+            onChange={formik.handleChange}
           />
-          <br />
+          {formik.touched.password && formik.errors.password ? (
+         <p className="error-message">
+           <strong>{formik.errors.password}</strong>
+           </p>
+       ) : null}
         </Form.Group>
         <Button type="submit">Sign Up</Button>
       </Form>
