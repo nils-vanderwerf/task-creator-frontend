31bd1bb32adcba78a0589b9fdfb99f01eac1383c
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-21 11:30:36 +1000

    Added validations

--- src/Validations/ValidationSchema.js
+++ src/Validations/ValidationSchema.js
@@ -1,16 +1,16 @@
 import * as yup from "yup"
 
-export const validationSchema = yup.object().shape({
+export const userValidationSchema = yup.object().shape({
     first_name: yup
         .string()
         .min(2, "Too short!")
         .max(50, "Too long!")
         .required("Required *"),
     last_name: yup
-    .string()
-    .min(2, "Too short!")
-    .max(50, "Too long!")
-    .required("Required *"),
+        .string()
+        .min(2, "Too short!")
+        .max(50, "Too long!")
+        .required("Required *"),
     email: yup
         .string()
         .email("Invalid email")
@@ -20,4 +20,16 @@ export const validationSchema = yup.object().shape({
         .min(4, "Too short!")
         .max(20, "Too long!")
         .required("Required *")
+})
+
+export const taskValidationSchema = yup.object().shape({
+    title: yup
+        .string()
+        .min(2, "Too short!")
+        .required("Required *"),
+    description: yup
+        .string()
+        .min(2, "Too short!")
+        .max(500, "Too long!")
+        .required("Required *"),
 })
