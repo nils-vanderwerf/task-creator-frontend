9604d12bd6420199a345a42571d0425ba52623c6
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-21 17:39:01 +1000

    Ready for deploy

--- src/components/pages/tasks/TaskItem.js
+++ src/components/pages/tasks/TaskItem.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 // import taskActions from '../../redux/actions/taskAction';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import taskActions from '../../../redux/actions/taskActions';
 import './Tasks.style.css'
 import ButtonContainer from './ButtonContainer';
