207200770268c83e9e4a99f268bb4c02a2656a89
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-26 10:51:11 +1000

    Revert "merge"
    This reverts commit 0c53f72f69daeef6425ecace5c0cfd281eaa2c48.

--- src/components/pages/tasks/ButtonContainer.js
+++ src/components/pages/tasks/ButtonContainer.js
@@ -4,6 +4,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 
+const ButtonContainer = ({ task }) => {
+    const tasks = useSelector(state => state.tasks)
+    const [showState, setShowState] = useContext(ShowModalContext)
+    const [confirmMessage, setConfirmMessage] = useContext(ConfirmMessageContext)
+    const [taskToDelete, setTaskToDelete] = useContext(TaskToDeleteContext)
+    const history = useHistory();
+
+    const dispatch = useDispatch()
+
+    const showModal = (event) => {
+        const targetInt = parseInt(event.target.id)
+        console.log("Task Int", targetInt)
+        const findTask = tasks.find(item => item["id"] === targetInt)
+        console.log("FIND TASK >> ", findTask)
+        setTaskToDelete(findTask)
+        setShowState(true)
+    }
+  
+    useEffect(() => {
+        console.log("Set task to delete", taskToDelete)
+    }, [taskToDelete])
+
+    const hideModal = () => setShowState(false)
+
+    const handleDeleteTask = () => {
+        dispatch(taskActions.deleteTaskFromDB(taskToDelete))
+        let taskMessage = document.getElementById('confirm-message')
+        setConfirmMessage(`Task '${taskToDelete.title}' has been deleted`)
+        hideModal()
+        dispatch(getAllTasks())
+        history.push('/');
+    }
+
 
 const ButtonContainer = ({ task, showModal }) => {
     return (
